39d456576bd6242e638ae9e31d796ec7b090db58
Mojken <mikaelgillis@gmail.com>
2019-04-17 18:22:34 +0200

    Add capabilities for multiple clients connected, change into multi-threaded approach.

--- src/main/java/networking/Server.java
+++ src/main/java/networking/Server.java
@@ -1,31 +1,56 @@
 package networking;
 
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 //Name: Torsten
 public class Server {
 
+    boolean running = false;
+
     private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+
+    private List<ServerClient> clients = new ArrayList<ServerClient>();
+    private Queue<String> messageQueue = new ConcurrentLinkedQueue<>();
 
     public void start(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        System.out.println(in.readLine());
-        stop();
+        running = true;
+        Server server = this;
+
+        new Thread ("Connection Listener") {
+            public void run() {
+                while (running) {
+                    try {
+                        ServerClient client = new ServerClient(serverSocket.accept());
+                        clients.add(client);
+                        client.receive(server);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
+    }
+
+    void queueAdd(String msg) {
+        messageQueue.add(msg);
+    }
+
+    public void toAll(String message) {
+        for (ServerClient client : clients) {
+            client.send(message);
+        }
     }
 
     public void stop() throws IOException {
-        in.close();
-        out.close();
-        clientSocket.close();
         serverSocket.close();
+        running = false;
     }
 
     public static void main(String[] args) {
--- src/main/java/networking/ServerClient.java
+++ src/main/java/networking/ServerClient.java
@@ -1,7 +1,62 @@
 package networking;
 
+import java.io.*;
+import java.net.Socket;
+
 public class ServerClient {
 
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    private boolean connected = false;
+    private final String IP;
 
 
+    ServerClient(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+
+        this.IP = clientSocket.toString();
+        System.out.println(IP);
+
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            connected = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //TODO: evaluate necessity of a thread here
+    public void send(String msg) {
+        out.println(msg);
+    }
+
+
+    void receive(Server server) {
+        new Thread(IP + " listener") {
+            public void run() {
+                while (connected) {
+                    try {
+                        server.queueAdd(in.readLine());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+
+                    }
+                }
+            }
+        }.start();
+    }
+
+    void disconnect() {
+        try {
+            connected = false;
+            out.close();
+            in.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
